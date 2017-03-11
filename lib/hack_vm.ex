36d8ce2d76f8003ddfbbfd26a3618170e4065ba9
Federico <ufinii@gmail.com>
2017-03-11 22:29:07 +0000

    First commit

---
+++ lib/hack_vm.ex
@@ -0,0 +1,18 @@
+defmodule HackVm do
+  @moduledoc """
+  Documentation for HackVm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HackVm.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hack_vm_test.exs
@@ -0,0 +1,8 @@
+defmodule HackVmTest do
+  use ExUnit.Case
+  doctest HackVm
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
