e4a9d473fc1c6a60304414e210c0f338bbc27926
Federico <ufinii@gmail.com>
2017-03-11 22:33:22 +0000

    ebuild script-ify

--- lib/hack_vm.ex
+++ lib/hack_vm.ex
@@ -1,18 +1,9 @@
 defmodule HackVm do
-  @moduledoc """
-  Documentation for HackVm.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HackVm.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(args \\ []) do
+    {opts, _, _} = OptionParser.parse(args,
+      switches: [start: :integer, end: :integer, src: :string, dest: :string, update_tags: :string],
+      aliases: [s: :start, e: :end, S: :src, d: :dest, u: :update_tags]
+      )
+    IO.puts "Not implemented yet"
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,10 +5,13 @@ defmodule HackVm.Mixfile do
     [app: :hack_vm,
      version: "0.1.0",
      elixir: "~> 1.4",
+     escript: escript,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
+
+  def escript, do: [main_module: HackVm]
 
   # Configuration for the OTP application
   #
