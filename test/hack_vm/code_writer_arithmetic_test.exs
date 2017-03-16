ec703ef28666ddb51e98c0014dc63094b9dd8be4
Federico Rebora <federico.rebora@carwow.co.uk>
2017-03-16 11:19:48 +0000

    Unnecessary tests, fixed test name

--- test/hack_vm/code_writer_arithmetic_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule HackVm.CodeWriterArithmeticTest do
-  use ExUnit.Case
-  alias HackVm.CodeWriter
-  alias HackVm.Parser
-
-  test "writes add code comment" do
-    assert true
-  end
-end
--- test/hack_vm/code_writer_stack_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule HackVm.CodeWriterStackTest do
-  use ExUnit.Case
-  alias HackVm.CodeWriter
-  alias HackVm.Parser
-
-  test "writes push code comment" do
-    assert true
-  end
-end
